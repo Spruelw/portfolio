6ccb70df1eef9c5b047faccef1a58be8033639c1
oospruel <137451524+oospruel@users.noreply.github.com>
2024-08-09 18:35:54 -0400

    update everything

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-var sections = document.querySelectorAll(".hide")
-
-
+var sections = document.querySelectorAll(".hide");
 
 let observer = new IntersectionObserver((entries, observer) => {
-    
-    entries.forEach((entry) =>{
-        if( entry.isIntersecting){
-            console.log(entry);
-            entry.target.classList.add("show")
-        }else{
-            entry.target.classList.remove("show")
-        }
-        
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log(entry);
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
 });
 
-sections.forEach((section)=>{
-  observer.observe(section)  
-})
+sections.forEach((section) => {
+  observer.observe(section);
+});
+
+var experienceList = document.querySelectorAll(".experience .item");
+var hoverGlowTimeout = 4000;
+
+function glow() {
+  var i = 1;
+  experienceList.forEach((item, i) => {
+    setTimeout(() => {
+      item.classList.add("glow");
+
+      setTimeout(() => {
+        item.classList.remove("glow");
+      }, 4000);
+      i++;
+    }, 4000 * i);
+  });
+}
+glow()
+setInterval(() => {
+    glow()
+}, 16000);
